5698c2f329d210221f8693052e2315ca82448b1b
Tim Neumann <neumantm@fius.informatik.uni-stuttgart.de>
2022-10-19 20:16:13 +0200

    improve(deleteOldBranches): Also delete branches only pulled for read
    Also does not need the hook anymore

--- deleteOldBranches.sh
+++ deleteOldBranches.sh
@@ -15,7 +15,8 @@ function printUsage() {
 }
 
 REMOTE_NAME="origin"
-DATA_DIR=".git/pushed_branches/"
+
+TMP_REMOTE_TRACKING_OIDS=".git/.deletetOldBranches_remote_tracking_oids/"
 
 includeUnpushed="false"
 
@@ -33,13 +34,31 @@ if [ $# -gt 0 ] ;then
   done
 fi
 
-if [ "$includeUnpushed" == "false" ] && ! test -d "$DATA_DIR" ;then
-  echo "The dir $DATA_DIR does not exist. It is needed to know which branch was pushed with what commit."
-  echo "Please install the apropriate push hook or use --include-unpushed."
-  exit 1
-fi
+default_branch="$(git symbolic-ref refs/remotes/origin/HEAD | sed 's@^refs/remotes/'"$REMOTE_NAME"'/@@')"
 
+# Store which branches were pushed/fetched with which oid
+rm -rf "$TMP_REMOTE_TRACKING_OIDS"
+while IFS= read -r branch; do
+  if upstream=$(git rev-parse "$branch@{u}" 2>/dev/null); then
+    mkdir --parents "$(dirname "$TMP_REMOTE_TRACKING_OIDS/$branch")"
+    echo "$upstream" > "$TMP_REMOTE_TRACKING_OIDS/$branch"
+  fi
+done < <(git branch --format '%(refname:lstrip=2)')
+
+# Delete all the origin/xyz branches
 git remote prune "$REMOTE_NAME"
+
+# Delete all the branches git can itself detect that they are merged.
+# These include branches that were merged without rebase or squash and branches without any commit made on them
+while IFS= read -r branch; do
+  if [ "$branch" == "$default_branch" ] ;then 
+    # Don't delete the default branch
+    continue
+  fi
+  git branch -d "$branch"
+done < <(git branch --format '%(refname:lstrip=2)' --merged)
+
+# Delete all branches where we do not find a reason to keep
 
 while IFS= read -r branch; do
   remoteExists="$(git ls-remote "$REMOTE_NAME" "$branch" | wc -l)"
@@ -48,12 +67,12 @@ while IFS= read -r branch; do
     continue
   fi
   if [ "$includeUnpushed" == "false" ] ;then
-    if ! test -f "$DATA_DIR/$branch" ;then
+    if ! test -f "$TMP_REMOTE_TRACKING_OIDS/$branch" ;then
       #Was never pushed, do not delete
       continue
     fi
     currentOid="$(git rev-parse "$branch")"
-    pushedOid="$(cat "$DATA_DIR/$branch")"
+    pushedOid="$(cat "$TMP_REMOTE_TRACKING_OIDS/$branch")"
     if [ "$currentOid" != "$pushedOid" ] ;then
       #Current commit is different from last pushed commit, do not delete
       continue
@@ -61,4 +80,3 @@ while IFS= read -r branch; do
   fi
   git branch -D "$branch"
 done < <(git branch --format '%(refname:lstrip=2)')
-
